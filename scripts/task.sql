fb9c710c58ddf654d8886b32c1eca6e12c93c7c0
leaf <yonghao@datastory.com.cn>
2018-10-15 13:12:00 +0800

    准备dao层的开发

---
+++ scripts/task.sql
@@ -0,0 +1,12 @@
+ #task表，用来存储J2EE平台插入其中的任务的信息
+ CREATE TABLE `task` (
+ `task_id` int(11) NOT NULL AUTO_INCREMENT,
+ `task_name` varchar(255) DEFAULT NULL,
+ `create_time` varchar(255) DEFAULT NULL,
+ `start_time` varchar(255) DEFAULT NULL,
+ `finish_time` varchar(255) DEFAULT NULL,
+ `task_type` varchar(255) DEFAULT NULL,
+ `task_status` varchar(255) DEFAULT NULL,
+ `task_param` text
+ PRIMARY KEY (`task_id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
---
+++ src/main/java/cn/xpleaf/netshop/serv/java/analysis/dao/ITaskDao.java
@@ -0,0 +1,11 @@
+package cn.xpleaf.netshop.serv.java.analysis.dao;
+
+/**
+ * @author xpleaf
+ * @date 2018/10/15 下午1:07
+ */
+public interface ITaskDao {
+
+
+
+}
